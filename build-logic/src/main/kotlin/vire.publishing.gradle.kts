61fdfee507dba334e1609356b97c839911f07873
Peter Smek <smek.peter@gmail.com>
2023-11-04 18:29:22 +0100

    Fix publish

--- build-logic/src/main/kotlin/vire.publishing.gradle.kts
+++ build-logic/src/main/kotlin/vire.publishing.gradle.kts
@@ -11,72 +11,72 @@ val javadocJar by tasks.register<Jar>("dokkaJavadocJar") {
     archiveClassifier.set("javadoc")
 }
 
- val sourcesJar by tasks.creating(Jar::class) {
-     archiveClassifier.set("sources")
-     from(sourceSets.getByName("main").allSource)
- }
+val sourcesJar by tasks.creating(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.getByName("main").allSource)
+}
 
- publishing {
-     repositories {
-         maven {
-             name = "GitHubPackages"
-             url = uri("https://maven.pkg.github.com/voxelpi/vire")
-             credentials {
-                 username = System.getenv("GITHUB_ACTOR")
-                 password = System.getenv("GITHUB_TOKEN")
-             }
-         }
-     }
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/voxelpi/vire")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 
-     publications {
-         create<MavenPublication>("maven") {
-             groupId = project.group.toString()
-             artifactId = project.name
-             version = project.version.toString()
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
 
-             from(components["kotlin"])
-             artifact(sourcesJar)
-             artifact(javadocJar)
+            from(components["kotlin"])
+            artifact(sourcesJar)
+            artifact(javadocJar)
 
-             pom {
-                 name = project.name
-                 description = project.description
-                 url = "https://github.com/voxelpi/vire"
+            pom {
+                name = project.name
+                description = project.description
+                url = "https://github.com/voxelpi/vire"
 
-                 licenses {
-                     license {
-                         name = "The MIT License"
-                         url = "https://opensource.org/licenses/MIT"
-                     }
-                 }
+                licenses {
+                    license {
+                        name = "The MIT License"
+                        url = "https://opensource.org/licenses/MIT"
+                    }
+                }
 
-                 developers {
-                     developer {
-                         id = "voxelpi"
-                         name = "Peter Smek"
-                         url = "https://voxelpi.net"
-                     }
-                 }
+                developers {
+                    developer {
+                        id = "voxelpi"
+                        name = "Peter Smek"
+                        url = "https://voxelpi.net"
+                    }
+                }
 
-                 scm {
-                     connection = "scm:git:https://github.com/voxelpi/vire.git"
-                     developerConnection = "scm:git:ssh://git@github.com/voxelpi/vire.git"
-                     url = "https://github.com/voxelpi/vire"
-                 }
+                scm {
+                    connection = "scm:git:https://github.com/voxelpi/vire.git"
+                    developerConnection = "scm:git:ssh://git@github.com/voxelpi/vire.git"
+                    url = "https://github.com/voxelpi/vire"
+                }
 
-                 issueManagement {
-                     system = "GitHub"
-                     url = "https://github.com/voxelpi/vire/issues"
-                 }
+                issueManagement {
+                    system = "GitHub"
+                    url = "https://github.com/voxelpi/vire/issues"
+                }
 
-                 ciManagement {
-                     system = "GitHub Actions"
-                     url = "https://github.com/voxelpi/vire/actions"
-                 }
-             }
-         }
-     }
- }
+                ciManagement {
+                    system = "GitHub Actions"
+                    url = "https://github.com/voxelpi/vire/actions"
+                }
+            }
+        }
+    }
+}
 
 signing {
     val signingSecretKey = System.getenv("SIGNING_KEY")
