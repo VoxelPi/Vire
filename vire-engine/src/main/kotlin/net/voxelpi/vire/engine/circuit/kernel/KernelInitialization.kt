e8007f43bf77688e5b7a9c9de6583d28c963f8ce
Peter Smek <smek.peter@gmail.com>
2024-04-23 00:25:51 +0200

    Added kernel initialization

---
+++ vire-engine/src/main/kotlin/net/voxelpi/vire/engine/circuit/kernel/KernelInitialization.kt
@@ -0,0 +1,52 @@
+package net.voxelpi.vire.engine.circuit.kernel
+
+import net.voxelpi.vire.engine.circuit.kernel.variable.Parameter
+import net.voxelpi.vire.engine.circuit.kernel.variable.Setting
+
+/**
+ * An instance of a kernel.
+ */
+public interface KernelInitialization {
+
+    /**
+     * The kernel which of which the instance was created.
+     */
+    public val kernel: Kernel
+
+    /**
+     * Returns the current value of the given [parameter].
+     *
+     * @param parameter the parameter of which the value should be returned.
+     */
+    public operator fun <T> get(parameter: Parameter<T>): T
+
+    /**
+     * Returns the current value of the given [setting].
+     *
+     * @param setting the parameter of which the value should be returned.
+     */
+    public operator fun <T> get(setting: Setting<T>): T
+
+    /**
+     * Sets the value of the given [setting] to the given [value].
+     *
+     * @param setting the parameter of which the value should be modified.
+     * @param value the new value of the parameter.
+     */
+    public operator fun <T> set(setting: Setting<T>, value: T)
+
+    /**
+     * Returns the current value of the parameter with the given [variableName].
+     *
+     * @param variableName the name of the parameter of which the value should be returned.
+     */
+    public operator fun get(variableName: String): Any?
+
+    /**
+     * Sets the value of the parameter with the given [parameterName] to the given [value].
+     *
+     * @param parameterName the name of the parameter of which the value should be modified.
+     * @param value the new value of the parameter.
+     */
+    public operator fun set(parameterName: String, value: Any?)
+}
--- vire-engine/src/main/kotlin/net/voxelpi/vire/engine/circuit/kernel/KernelState.kt
+++ vire-engine/src/main/kotlin/net/voxelpi/vire/engine/circuit/kernel/KernelState.kt
@@ -1,3 +1,71 @@
 package net.voxelpi.vire.engine.circuit.kernel
 
-public interface KernelState
+public interface KernelState {
+
+    public companion object {
+
+        /**
+         * Creates a new state of the given [kernelInstance].
+         * @param kernelInstance the kernelInstance of which a new state should be created.
+         */
+        public fun create(kernelInstance: KernelInstance): KernelState {
+            require(kernelInstance is KernelInstanceImpl)
+            return KernelStateImpl.create(kernelInstance)
+        }
+
+        /**
+         * Creates a new state of the given [kernelInstance] that is initialized using the given [block].
+         * Values are initialized to their default values before the block is applied.
+         * @param kernelInstance the kernelInstance of which a new state should be created.
+         * @param block the code that should be applied to the kernel configuration.
+         */
+        public fun create(kernelInstance: KernelInstance, block: KernelInitialization.() -> Unit): KernelState {
+            require(kernelInstance is KernelInstanceImpl)
+            return KernelStateImpl.create(kernelInstance, block)
+        }
+
+        /**
+         * Creates a new state of the given [kernelInstance] that is initialized using the given [values].
+         * Values are initialized to their default values if not specified in the values map.
+         * @param kernelInstance the kernelInstance of which a new state should be created.
+         * @param values the values that should be applied to the kernel configuration.
+         */
+        public fun create(kernelInstance: KernelInstance, values: Map<String, Any?>): KernelState {
+            require(kernelInstance is KernelInstanceImpl)
+            return KernelStateImpl.create(kernelInstance, values)
+        }
+    }
+}
+
+internal class KernelStateImpl : KernelState {
+    companion object {
+
+        fun create(kernelInstance: KernelInstanceImpl): KernelStateImpl {
+//        val state = KernelStateImpl(kernelInstance, kernel.generateDefaultConfiguration())
+//        return state
+            TODO()
+        }
+
+        fun create(kernelInstance: KernelInstanceImpl, block: KernelInitialization.() -> Unit): KernelStateImpl {
+//        val config = kernel.generateDefaultConfiguration()
+//        config.block()
+//        return KernelStateImpl(kernelInstance, config)
+            TODO()
+        }
+
+        fun create(kernelInstance: KernelInstanceImpl, values: Map<String, Any?>): KernelStateImpl {
+//        val config = kernelInstance.generateDefaultConfiguration()
+//        for ((parameterName, parameterValue) in values) {
+//            // Check that only existing parameters are specified.
+//            val parameter = kernelInstance.parameter(parameterName)
+//                ?: throw IllegalArgumentException("Unknown parameter '$parameterName'")
+//
+//            // Check that the value is valid for the parameter.
+//            require(parameter.isValidValue(parameterValue)) { "Invalid value for the parameter ${parameter.name}" }
+//            config[parameterName] = parameterValue
+//        }
+//        return KernelStateImpl(kernel, config)
+            TODO()
+        }
+    }
+}
