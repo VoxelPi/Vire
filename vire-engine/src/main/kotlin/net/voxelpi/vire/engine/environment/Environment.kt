517a19a448b78dacd68b154d443c81f7d68332e7
Peter Smek <smek.peter@gmail.com>
2024-05-13 02:07:55 +0200

    Added methods to get registered kernels of an environment

--- vire-engine/src/main/kotlin/net/voxelpi/vire/engine/environment/Environment.kt
+++ vire-engine/src/main/kotlin/net/voxelpi/vire/engine/environment/Environment.kt
@@ -6,6 +6,7 @@ import net.voxelpi.vire.engine.Identifier
 import net.voxelpi.vire.engine.circuit.Circuit
 import net.voxelpi.vire.engine.circuit.CircuitImpl
 import net.voxelpi.vire.engine.environment.library.Library
+import net.voxelpi.vire.engine.kernel.Kernel
 import net.voxelpi.vire.engine.kernel.KernelInstance
 import net.voxelpi.vire.engine.kernel.KernelInstanceImpl
 import net.voxelpi.vire.engine.simulation.Simulation
@@ -27,9 +28,19 @@ public interface Environment {
     public fun libraries(): Collection<Library>
 
     /**
-     * Returns the library with the given id.
+     * Returns the library with the given [id].
      */
     public fun library(id: String): Library?
+
+    /**
+     * All kernels that are registered in the environment.
+     */
+    public fun kernels(): Collection<Kernel>
+
+    /**
+     * Returns the kernel with the given [id].
+     */
+    public fun kernel(id: Identifier): Kernel?
 
     /**
      * Creates a new circuit.
@@ -47,6 +58,7 @@ internal class EnvironmentImpl(libraries: List<Library>) : Environment {
     override val eventScope: EventScope = eventScope()
 
     private val libraries = libraries.associateBy { it.id }
+    private val kernels: Map<Identifier, Kernel> = libraries.map { it.kernels() }.flatten().associateBy { it.id }
 
     override fun libraries(): Collection<Library> {
         return libraries.values
@@ -54,6 +66,14 @@ internal class EnvironmentImpl(libraries: List<Library>) : Environment {
 
     override fun library(id: String): Library? {
         return libraries[id]
+    }
+
+    override fun kernels(): Collection<Kernel> {
+        return kernels.values
+    }
+
+    override fun kernel(id: Identifier): Kernel? {
+        return kernels[id]
     }
 
     override fun createCircuit(id: Identifier): Circuit {
