7ec4254adfea93185960a6ec15f2c49a5002ac28
Peter Smek <smek.peter@gmail.com>
2024-05-30 15:02:41 +0200

    Fix IO kernels

--- vire-stdlib/src/main/kotlin/net/voxelpi/vire/stdlib/kernel/IO.kt
+++ vire-stdlib/src/main/kotlin/net/voxelpi/vire/stdlib/kernel/IO.kt
@@ -2,7 +2,6 @@ package net.voxelpi.vire.stdlib.kernel
 
 import net.voxelpi.vire.engine.Identifier
 import net.voxelpi.vire.engine.LogicState
-import net.voxelpi.vire.engine.LogicValue
 import net.voxelpi.vire.engine.kernel.Kernel
 import net.voxelpi.vire.engine.kernel.KernelProvider
 import net.voxelpi.vire.engine.kernel.kernel
@@ -10,21 +9,16 @@ import net.voxelpi.vire.engine.kernel.variable.InputScalar
 import net.voxelpi.vire.engine.kernel.variable.OutputScalar
 import net.voxelpi.vire.engine.kernel.variable.Parameter
 import net.voxelpi.vire.engine.kernel.variable.input
-import net.voxelpi.vire.engine.kernel.variable.min
 import net.voxelpi.vire.engine.kernel.variable.output
 import net.voxelpi.vire.engine.kernel.variable.parameter
 import net.voxelpi.vire.stdlib.VIRE_STDLIB_ID
 
 public object Output : KernelProvider {
-    public val value: Parameter<LogicValue> = parameter("value", initialization = { LogicValue.NONE })
-    public val channels: Parameter<Int> = parameter("channels", initialization = { 1 }) {
-        min(1)
-    }
-    public val output: OutputScalar = output("output") { LogicState.value(this[value], this[channels]) }
+    public val value: Parameter<LogicState> = parameter("value", initialization = { LogicState.EMPTY })
+    public val output: OutputScalar = output("output") { this[value] }
 
-    override val kernel: Kernel = kernel(Identifier(VIRE_STDLIB_ID, "input")) {
+    override val kernel: Kernel = kernel(Identifier(VIRE_STDLIB_ID, "output")) {
         declare(value)
-        declare(channels)
         declare(output)
     }
 }
@@ -32,7 +26,7 @@ public object Output : KernelProvider {
 public object Input : KernelProvider {
     public val input: InputScalar = input("input")
 
-    override val kernel: Kernel = kernel(Identifier(VIRE_STDLIB_ID, "output")) {
+    override val kernel: Kernel = kernel(Identifier(VIRE_STDLIB_ID, "input")) {
         declare(input)
     }
 }
